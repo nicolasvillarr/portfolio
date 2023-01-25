813db735fc9d269cb5b53fa4eba655d9c3d0ea31
Nicolas Villar <104827186+nicolasvillarr@users.noreply.github.com>
2023-01-25 04:36:59 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
     <div className='contAbout'>
       <div>
       <h1 className='titleAbout'> SOBRE MÍ</h1>
-        <p className='parrafo'>Hola! Me llamo Pablo Nicolas Villar, nací el 22 de noviembre de 2002 en  <strong>Argentina</strong>. Desde muy pequeño me gusto la tecnología y los viedojuegos, me considero una persona perseverante. Me gusta trabajar en equipo y aprender cosas nuevas, soy desarrollador web Full Stack graduado en el curso de Soy Henry.
+        <p className='parrafo'>Hola! Me llamo Pablo Nicolas Villar, nací el 22 de noviembre de 2002 en  <strong>Argentina</strong>. Desde muy pequeño me gusto la tecnología, me considero una persona perseverante. Me gusta trabajar en equipo y aprender cosas nuevas, soy desarrollador web Full Stack graduado en el curso de Soy Henry.
         Si te interesa mi perfil te dejo mi CV para que puedas ver más detalles sobre mí
         <div>‎</div>
 <a className='linkCV' href={cv}>
